e7d96c43b11f3d48dd74d2a1dfb6378a2991e08c
leog10 <leonardogilli@outlook.com>
2023-01-26 19:14:57 -0300

    Updated selectCards

--- src/sockets/Cards/selectCard.ts
+++ src/sockets/Cards/selectCard.ts
@@ -5,37 +5,29 @@ export default (io: Server, client: Socket & { sessionId?: string }) => {
   client.on('client:card_select', async ({ card, roomId, clientId }) => {
     console.log('Client select card', roomId, card, clientId, client.sessionId);
 
+    await Room.updateOne(
+      {
+        _id: roomId
+      },
+      {
+        $set: {
+          'users.$[e1].card': card,
+          'voting.$[e1].card': card
+        }
+      },
+      {
+        arrayFilters: [{ 'e1.clientId': client.sessionId }]
+      }
+    );
+
     const room = await Room.findById(roomId);
 
     if (!room) {
+      console.log('room not found on card select');
       return;
     }
-
-    const userIndex = room.users.findIndex(user => user.clientId === clientId);
-    const userVotingIndex = room.voting.findIndex(
-      user => user.clientId === clientId
-    );
-
-    if (userIndex === -1) {
-      return;
-    }
-
-    room.users[userIndex].card = card;
-
-    if (userVotingIndex === -1) {
-      room.voting.push(room.users[userIndex]);
-    } else {
-      room.voting[userVotingIndex].card = card;
-    }
-
-    await room.save();
 
     io.to(roomId).emit('server:users', {
-      roomVoting: room.voting,
-      reveal: room.reveal
-    });
-
-    client.emit('server:users', {
       roomVoting: room.voting,
       reveal: room.reveal
     });
